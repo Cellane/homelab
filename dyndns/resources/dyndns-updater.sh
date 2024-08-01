05ef728a7ce332721d32e16097199e7592948e0f
Milan Vit <milanvit@milanvit.net>
2024-08-01 12:19:12 +0900

    I wish Google could stop being a fucking retard for one goddamn second

--- dyndns/resources/dyndns-updater.sh
+++ dyndns/resources/dyndns-updater.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+PUBLIC_IP=$(curl -s -4 https://api.ipify.org)
+
 curl -X PATCH "https://api.cloudflare.com/client/v4/zones/$ZONE_ID/dns_records/$RECORD_ID" \
   -H "Authorization: Bearer $API_TOKEN" \
   -H "Content-Type: application/json" \
-  --data "{\"content\": \"$(curl -s -4 https://domains.google.com/checkip)\"}"
+  --data "{\"content\": \"$PUBLIC_IP\"}"
